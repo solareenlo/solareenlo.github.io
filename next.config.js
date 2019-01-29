f10008844ece0a9653899cc315a41c90428df8d8
solareenlo <solareenlo@gmail.com>
2019-01-29 09:26:02 +0900

    fix typo

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ module.exports = {
     return {
       "/": { page: "/" },
       "/about": { page: "/about" },
+      '/p/hello-nextjs': { page: '/post', query: { title: 'Hello Next.js' } },
+      '/p/learn-nextjs': { page: '/post', query: { title: 'Learn Next.js is awesome' } },
+      '/p/deploy-nextjs': { page: '/post', query: { title: 'Deploy apps with Zeit' } },
+      '/p/exporting-pages': { page: '/post', query: { title: 'Learn to Export HTML Pages' } }
     }
   },
   //assetPrefix: '',
