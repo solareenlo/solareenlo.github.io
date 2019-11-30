5a27cff3627e1ea9be4f1bab80d893ef901c9f46
solareenlo <solareenlo@gmail.com>
2019-12-01 00:01:30 +0900

    rebuilding site 2019年 12月 1日 日曜日 00時01分30秒 JST

---
+++ en.search.min.25446b14ce8f9c6c367c8f28ce2f2c373734aef49688c718f5390b50af60648f.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.028482a0de6cf49213ac55a081b74dd0e4a0985f44bbefb1b4e7d2f377924079.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
